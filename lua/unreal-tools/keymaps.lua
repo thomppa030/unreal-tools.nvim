e5ee7cd926d93e1694001c6e553054bb06122579
Kay Priesnitz <kpriesnitz@outlook.de>
2025-02-27 22:01:34 +0100

    UPDATE Keymaps to include standard lsp keymappings
    This is purely temporarily, as this should never overwrite user lsp
    setting.

--- lua/unreal-tools/keymaps.lua
+++ lua/unreal-tools/keymaps.lua
@@ -3,13 +3,25 @@ local M = {}
 function M.setup(config, project)
   local prefix = config.keymap_prefix
 
-  vim.keymap.set('n', prefix .. 'os', 'UEOpenSourceDir<CR>', {desc = 'Open Source directory'})
-  vim.keymap.set('n', prefix .. 'oc', 'UEOpenContentDir<CR>', {desc = 'Open Source directory'})
+  vim.keymap.set('n', prefix .. 'os', 'UEOpenSourceDir<CR>', { desc = 'Open Source directory' })
+  vim.keymap.set('n', prefix .. 'oc', 'UEOpenContentDir<CR>', { desc = 'Open Source directory' })
 
-  vim.keymap.set('n', prefix .. 'gc', 'UEGenerateCompileCommands<CR>', {desc = 'Generates UE compile commands'})
+  vim.keymap.set('n', prefix .. 'gc', 'UEGenerateCompileCommands<CR>', { desc = 'Generates UE compile commands' })
 
-  vim.keymap.set('n', prefix .. 'hs', 'UESwitchHeaderSource<CR>', {desc = 'Switches between Header/Source file'})
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 
+  vim.keymap.set('n', '<leader>-', ':b#<CR>', { noremap = true, silent = true, desc = "Switch Back to last used Buffer"})
+
+
+  vim.keymap.set('n', prefix .. 'hs', 'UESwitchHeaderSource<CR>', { desc = 'Switches between Header/Source file' })
 end
 
 return M
